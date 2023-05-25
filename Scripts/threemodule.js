efc5e89d083d9a53fbaea8b2259af76669594d4c
SeaRyanC <searyanca@users.noreply.github.com>
2023-05-25 07:59:35 -0700

    Reorg pt 1

--- Scripts/threemodule.js
+++
@@ -1,5 +0,0 @@
-﻿// Type definitions for three.js -- r66
-// Project: http://mrdoob.github.com/three.js/
-// Definitions by: Kon <http://phyzkit.net/>, Satoru Kimura <https://github.com/gyohk>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-//# sourceMappingURL=threemodule.js.map
--- packages/rangoric.three.ts.0.66.4/content/Scripts/threemodule.ts
+++
@@ -1,3 +0,0 @@
-declare var THREE: any;
-
-export = THREE;
